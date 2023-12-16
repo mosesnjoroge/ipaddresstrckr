7786e5bf4dae22a00fe2157d0ba927809d677ddd
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-12-16 18:38:50 -0500

    using bootstrap stack temp

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -2,40 +2,40 @@
 export default function InfoBox() {
   return(
     <div className="container">
-      <div className="card-category">
+      <div className="card-category hstack gap-5">
         <div className="location-box">
           <h6>
             IP ADDRESS
           </h6>
           <h3>
-
+            testing
           </h3>
         </div>
-        <hr className="box-solid-divider"/>
+        <div className="vr"/>
         <div className="ip-address-box">
           <h6>
             LOCATION
           </h6>
           <h3>
-
+            testing
           </h3>
         </div>
-        <hr className="box-solid-divider"/>
+        <div className="vr"/>
         <div className="timezone-box">
           <h6>
             TIMEZONE
           </h6>
           <h3>
-
+            testing
           </h3>
         </div>
-        <hr className="box-solid-divider"/>
+        <div className="vr"/>
         <div className="isp-box">
           <h6>
             ISP
           </h6>
           <h3>
-
+            testing
           </h3>
         </div>
       </div>
