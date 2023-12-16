0cc8222a9df97a55068d8128ba3836a5f8871c87
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-12-16 17:45:12 -0500

    aligned info-box properly

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -3,7 +3,29 @@ export default function InfoBox() {
   return(
     <div className="container">
       <div className="card-category">
-
+        <div className="location-box">
+          <h2>
+            IP ADDRESS
+          </h2>
+        </div>
+        <hr className="box-solid-divider"/>
+        <div className="ip-address-box">
+          <h2>
+            LOCATION
+          </h2>
+        </div>
+        <hr className="box-solid-divider"/>
+        <div className="timezone-box">
+          <h2>
+            TIMEZONE
+          </h2>
+        </div>
+        <hr className="box-solid-divider"/>
+        <div className="isp-box">
+          <h2>
+            ISP
+          </h2>
+        </div>
       </div>
       {/* <div className="cards">
         <div className="card-category">
