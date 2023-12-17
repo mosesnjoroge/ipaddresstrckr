afc36ecc49ad1e242f1984585eb55ef5f3fe74c6
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-12-16 20:38:44 -0500

    search form functional in es6 react form

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,6 +9,10 @@ export default function Banner() {
     setSearchInput(e.target.value);
     console.log(setSearchInput)
   }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchInput)
+  }
   /* Select form */
   // const search_form = document.querySelectorByClass(".map-form");
 
@@ -36,15 +40,16 @@ export default function Banner() {
         <h2>
           IP Address Tracker
         </h2>
-        <form className="map-form">
+        <form className="map-form" onSubmit={handleSubmit}>
           <div className="mb-3 d-flex">
             <input
               className="form-control w-70"
               type="text"
               placeholder="Search for any IP address or domain"
               id="search"
+              value={searchInput}
               onChange={handleChange}
-              value={searchInput}/>
+            />
             <button type="submit" className="btn btn-outline-primary" alt="">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right" viewBox="0 0 16 16">
                 <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
