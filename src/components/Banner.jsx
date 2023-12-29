5a5b12ae32572b87461e029cd195a611b71bd46d
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-12-29 18:18:48 -0500

    reintegrated form to banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,7 @@
 import Map from './MapComponents/Map'
 import InfoBox from './MapComponents/InfoBox';
 
-
-export default function Banner() {
+function Banner() {
 
   return(
     <div className="banner-search-section">
@@ -10,17 +9,28 @@ export default function Banner() {
         IP Address Tracker
       </h2>
       <form>
-        <input
-
-        />
-        <button>
-
-        </button>
+        <div className="map-form">
+          <input
+            type="text"
+            placeholder="Search for any IP address or domain"
+            className="input"
+            id="search"
+            // value={searchInput}
+            // onChange={handleChange}
+          />
+          <button type="submit" alt="" className="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right" viewBox="0 0 16 16">
+              <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
+            </svg>
+          </button>
+        </div>
       </form>
       <InfoBox/>
       <div id='map'>
         <Map/>
       </div>
     </div>
-  )
+  );
 }
+
+export default Banner
