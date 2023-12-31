ca42fd9096b1f90e7ac57612a5190a0ef1c0b9cb
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-12-30 20:57:23 -0500

    useeffect correction

--- src/components/MapComponents/searchForm.jsx
+++ src/components/MapComponents/searchForm.jsx
@@ -10,7 +10,7 @@ export default function SearchForm(){
   // use effect for data retrieval
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios('https://geo.ipify.org/api/v2/country,city?apiKey=at_9zLazwhedqY57YjmEW8ZCBzKBg3MI&ipAddress=8.8.8',
+      const result = await axios(`https://geo.ipify.org/api/v2/country,city?apiKey=at_9zLazwhedqY57YjmEW8ZCBzKBg3MI&ipAddress=${query}`,
       );
       setData(result.data);
           // .then((response) => response.json())
@@ -30,7 +30,7 @@ export default function SearchForm(){
 
   // get input value and store in state
   const handleChange = (e) => {
-    setQuery(e.target.value);
+    query(e.target.value);
   };
 
   // handle submit
@@ -52,6 +52,19 @@ export default function SearchForm(){
             value={query}
             onChange={handleChange}
           />
+          <div className="box">
+            {data.hits.map(item => (
+                <a href={item.url}>{item.title}</a>
+              </li>
+              <h6>
+                IP ADDRESS
+              </h6>
+              <h3 className="address" key={item.ip}>
+
+              </h3>
+            ))}
+          </div>
+
           <button type="submit" alt="" className="button">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right" viewBox="0 0 16 16">
               <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
