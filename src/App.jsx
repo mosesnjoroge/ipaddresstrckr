95f446e211a3c2e1765e1109dab445e4eb0bc7a1
mosesnjoroge <mosesbnjoroge@outlook.com>
2024-01-05 13:16:58 -0500

    form submit works through enter works: debug submit button

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
    const apiReq = (req = "") => {
     axios
       .get(
-        `https://geo.ipify.org/api/v2/country,city?apiKey=at_9zLazwhedqY57YjmEW8ZCBzKBg3MI&domain=${req}`
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_9zLazwhedqY57YjmEW8ZCBzKBg3MI&ipAddress=${req}`
       )
       .then((res) => {
         console.log(setIp(res.data));
